94a3cfc46f8930503f85fe7ace6eabfcbd48c5a7
mummysboy <isaachirsch@gmail.com>
2024-11-07 15:57:07 -0800

    fixed import app
    hopefully

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.js";
 import { FormDataProvider } from "./contexts/FormDataContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
